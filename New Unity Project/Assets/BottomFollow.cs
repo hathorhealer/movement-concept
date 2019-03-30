d6f1b9f23eead5495a19cf9997428363b5083e36
hathorhealer <49064814+hathorhealer@users.noreply.github.com>
2019-03-30 18:27:41 +0000

    Double Jumping and proper ground detection

---
+++ New Unity Project/Assets/BottomFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class BottomFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = desiredPosition;
+    }
+
+}
